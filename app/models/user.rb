836db83f78a041d9fa64f52cbaec5fa7f26459d2
Sarah <sarahkane682@yahoo.com>
2016-05-22 16:14:45 -0700

    comminging more stuff I think in got the image or somthing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   validates  :uid, :provider, presence: true
 
   def self.find_or_create_from_omniauth(auth_hash)
-    raise
     # Find or create a user
     user = self.find_by(uid: auth_hash["info"].id, provider: auth_hash["provider"])
     if !user.nil?
